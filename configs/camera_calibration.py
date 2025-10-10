2eb4e5aa3b0018cd84360591db9dc34a6dd33ef1
François Proulx <francois.proulx@usherbrooke.ca>
2025-10-10 19:07:07 -0400

    configs modifs for camera

--- configs/camera_calibration.py
+++ configs/camera_calibration.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import glob
 
-def calibrate_camera(chessboard_size=(9,6), square_size=0.024, save_path='camera_intrinsics.npz'):
+def calibrate_camera(chessboard_size=(8,6), square_size=0.02275, save_path='configs/camera_intrinsics.npz'):
     """
     Calibrate camera using chessboard images.
     
@@ -19,17 +19,28 @@ def calibrate_camera(chessboard_size=(9,6), square_size=0.024, save_path='camera
     objpoints = []
     imgpoints = []
 
-    images = glob.glob('configs/calib_images/*.jpg')
-    for fname in images:
+    images = glob.glob('configs/calib_images/*.jpg')  # video_20251010_180006_frames
+    print(f"Found {len(images)} images for calibration.")
+    for idx, fname in enumerate(images):
         img = cv2.imread(fname)
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         ret, corners = cv2.findChessboardCorners(gray, chessboard_size, None)
         if ret:
             objpoints.append(objp)
             imgpoints.append(corners)
+            print(f"[{idx+1}/{len(images)}] Chessboard detected in {fname}")
+        else:
+            print(f"[{idx+1}/{len(images)}] No chessboard found in {fname}")
+    
+    if len(objpoints) == 0:
+        raise RuntimeError("No chessboard corners were found in any image. Calibration failed.")
     
     ret, camera_matrix, dist_coeffs, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
     
     np.savez(save_path, camera_matrix=camera_matrix, dist_coeffs=dist_coeffs)
     print(f"Calibration done. Saved to {save_path}")
     return camera_matrix, dist_coeffs
+
+
+if __name__ == "__main__":
+    calibrate_camera()
